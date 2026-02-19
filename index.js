e068deaa6eed3dfca14473196b01c54fe1b677a9
Emauay Palmer <emaujay.palmer@student.swic.edu>
2026-02-19 02:08:57 -0600

    Add personal information variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello via Bun (plain JS)!");
+const firstName = "E'Maujay";
+const lastName = "Palmer";
+const age = 20; // Your actual age
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is E'Maujay Palmer. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
